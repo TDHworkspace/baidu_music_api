c655fb93a29c4c1d7790d663370cacebe888fca2
ci_knight <ci_knight@msn.cn>
2014-11-09 10:55:54 +0800

    优化一些参数 Signed-off-by: ci_knight <ci_knight@msn.cn>

--- baidu_music_api.py
+++ baidu_music_api.py
@@ -9,34 +9,31 @@ import urllib2
 import time
 import json
 class Baidu_Music():
-#--------------获取搜索音乐JSON------------------
-    def Input_SongName(self):
-        print u'请输入要搜索的音乐'
-        query = raw_input()
-        val = {}
-        val['from'] = 'webapp_music'
-        val['format'] = 'json'
-        val['callback'] = ''
-        val['_'] = int(time.time())
-        val['method'] = 'baidu.ting.search.catalogSug'
-        val['query'] = query
-#        val['query'] = '红玫瑰' #测试使用
-        val['size'] = '1'
-#API_URL,处理URL参数
-        url = 'http://tingapi.ting.baidu.com/v1/restserver/ting'
-        url_values = urllib.urlencode(val)
+#---------初始化公共参数---------
+    def __init__(self):
+        self.val = {'from': 'webapp_music',
+                    'format': 'json',
+                    'callback': '',
+                    '_': int(time.time()),
+                    '_t' : int(time.time()),
+                    'method': 'baidu.ting.search.catalogSug',
+                    }
 #定义HEADER
         user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
-        header = {'User-Agent': user_agent}
+        self.header = {'User-Agent': user_agent}
+#--------------获取搜索音乐JSON------------------
+    def Input_SongName(self):
+        query = raw_input(u'请输入要搜索的音乐')
+        self.val['query'] = query
+        self.val['size'] = '1'
+#API_URL,处理URL参数
+        url = 'http://tingapi.ting.baidu.com/v1/restserver/ting'
+        url_values = urllib.urlencode(self.val)
 #发送REQUEST请求
-        req = urllib2.Request(url, url_values, header)
+        req = urllib2.Request(url, url_values, self.header)
 #得到RESPONSE响应
         response = urllib2.urlopen(req,timeout=10) #10秒后timeout
         jsons = response.read()
-#    print jsons
-#f = open('1.mp3','w+')
-#f = f.write(html)
-#print html
 ###################处理JSON#################
 #加载JSON数据
         j = json.loads(jsons)
@@ -50,7 +47,7 @@ class Baidu_Music():
                 songid.append(j['song'][i]['songid'])
                 artistname.append(j['song'][i]['artistname'])
         except:
-            print u'不需理会异常'
+            print u'列表越界,最多显示9个'
         print u'############获取列表成功##############'
         return songname,songid,artistname
 #------------下载函数-------------
@@ -60,36 +57,27 @@ class Baidu_Music():
         print u'下载完成,已保持到当前目录下'
 #-------------获取下载url地址-------------
     def Get_MusicUrl(self, song_num):
-        val = {}
-        val['format'] = 'json'
-        val['callback'] = '__.cb_download'
-        val['songIds'] = song_num
-        val['rate'] = '320'
-        val['_'] = int(time.time())
-        val['_t'] = int(time.time())
-        val['type'] = 'flac,mp3'
+        self.val['callback'] = '__.cb_download'
+        self.val['rate']='320'
+        self.val['songIds'] = song_num
+        self.val['type'] = 'flac,mp3'
 #API_URL,处理URL参数
         url = 'http://music.baidu.com/data/music/fmlink'
-        url_values = urllib.urlencode(val)
-#        print url+"?"+url_values
-#定义HEADER
-        user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
-        header = {'User-Agent': user_agent}
+        url_values = urllib.urlencode(self.val)
 #发送REQUEST请求
-        req = urllib2.Request(url, url_values, header)
+        req = urllib2.Request(url, url_values, self.header)
 #得到RESPONSE响应
         response = urllib2.urlopen(req,timeout=10) #10秒后timeout
         jsons = response.read()
         jsons = jsons.replace('__cb_download(','')
         jsons = jsons.replace('}]}})','}]}}')
         jsons = jsons.replace('\\','')
-#        print jsons
 ###################处理JSON#################
 #加载JSON数据
         j = json.loads(jsons)
-        try:
-               songLinks =  j['data']['songList'][0]['songLink']
-               formats = j['data']['songList'][0]['format']
+        try:    
+            songLinks =  j['data']['songList'][0]['songLink']
+            formats = j['data']['songList'][0]['format']
         except:
             print u'获取下载地址失败'
         return songLinks,formats
@@ -97,14 +85,11 @@ class Baidu_Music():
 Music = Baidu_Music()
 songname,songid,artistname = Music.Input_SongName()
 for i in range(0,len(songname)):
-    print str(i+1)+'. '+songname[i] + '--' + artistname[i]+'  id: '+songid[i]
+    print str(i+1)+'. '+songname[i] + '--' + artistname[i]
 #输入音乐编号
 print u'#####################################'
-print u'输入音乐编号,进行下载'
-song_num = raw_input()
+song_num = raw_input(u'输入音乐编号,进行下载')
 song_name = songname[int(song_num)-1] 
 song_num = songid[int(song_num)-1]
 songLink, formats = Music.Get_MusicUrl(song_num)
 Music.Down_Music(songLink, song_name, formats)
-
-
