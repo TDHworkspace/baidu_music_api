e626df84386612dabe8ac2ff2c246e13a1661fba
ci_knight <ci_knight@msn.cn>
2014-11-10 16:32:47 +0800

    bug bug bug
    Signed-off-by: ci_knight <ci_knight@msn.cn>

--- wangyi_music_api.py
+++ wangyi_music_api.py
@@ -1,4 +1,4 @@
-﻿#coding=utf-8
+﻿#encoding: utf-8
 '''
 Created on 2014年11月9日
 
@@ -22,7 +22,7 @@ def Get_WangYi_Music(s):
     url = 'http://s.music.163.com/search/get/'
     url_values = urllib.urlencode(val)
     user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
-    header = {#'User-Agent': user_agent,
+    header = {'User-Agent': user_agent,
               #'Cookie': 'appver=1.5.2',
               }
     print url+'?'+url_values
@@ -35,14 +35,16 @@ def Get_WangYi_Music(s):
 ###################处理JSON#################
 #加载JSON数据
     j = json.loads(jsons)
+    num = int(j['result']['songCount'])
+    print num
+    link = []
     try:
-        print j['result']['songCount']  
-        print j['result']['songs'][0]['audio']
-        print j['result']['songs'][0]['artists'][0]['name']
-        print j['result']['songs'][0]['name']
+        for i in range(10):
+            print str(i)+'. '+j['result']['songs'][i]['name']+'-'+j['result']['songs'][i]['artists'][0]['name']
+            link.append(j['result']['songs'][i]['audio'])
     except:
         print u'获取下载地址失败'
-    return j['result']['songs'][0]['audio']
+    return link
 
 def playMp3(filename):
     #filename = r'one year.mp3'
@@ -53,5 +55,9 @@ def playMp3(filename):
         print mp3.seconds()
         time.sleep(mp3.seconds())
         mp3.stop()
-filename = Get_WangYi_Music(raw_input(u'输入歌名'))
-playMp3(filename)
+print u'输入歌名'
+name = raw_input()
+filelist = Get_WangYi_Music(name)
+print u'输入编号'
+num = int(raw_input())
+playMp3(filelist[num])
